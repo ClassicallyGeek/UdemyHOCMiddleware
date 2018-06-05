3c6d8fdbc5cf38da3e1b372934dd1fecc11567bd
Ashley <AshleyReneeLowry@gmail.com>
2018-06-05 16:51:43 -0500

    state validator updates

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -4,7 +4,7 @@ import stateSchema from 'middlewares/stateSchema';
 // When the state updates (post middlewares & reducers) we want to validate it.
 // getState returns all the state in the Redux Store.
 export default({ dispatch, getState }) => (next) => (action) => {
-  next(action);
+  next(action); // TODO why isn't this an infinite loop? 
 
   if(!tv4.validate(getState(), stateSchema)){
     console.warn('Invalid state schema detected');
