b7966590db8c5c61ca8781393b7d71e1d7e8be46
Ashley <AshleyReneeLowry@gmail.com>
2018-06-05 10:11:00 -0500

    Adding stateValidtor middleware

--- src/Root.js
+++ src/Root.js
@@ -6,10 +6,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 
 // initialState - an optional starting State. Helpful for Testing.
 export default ({children, initialState = {}}) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator));
   return (
     <Provider store={ store } >
       { children }
