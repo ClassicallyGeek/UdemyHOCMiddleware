90122f8ef84a4869997ff23b196bb80c02fa703a
Ashley <AshleyReneeLowry@gmail.com>
2018-06-05 06:45:51 -0500

    Adding a header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-  // exact prop in the route won't greedy match
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
+class App extends Component {
+
+  renderButton() {
+    if( this.props.auth) {
+      return (
+        <button>Sign Out</button>
+      );
+    } else {
+      return (
+        <button>Sign In</button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/post">Post</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    // exact prop in the route won't greedy match
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 }
+
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(App)
